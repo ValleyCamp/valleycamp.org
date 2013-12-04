1839777e48ae3b71cc6a42c90c34a2fbc5d796a2
Josh Proehl <josh@daedalusdreams.com>
2013-12-03 20:01:29 -0800

    Bad developer Update stylesheets and background images, change to SASS

--- lib/default.rb
+++ lib/default.rb
@@ -1,2 +1,7 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
+
+# An easy way to get a link to layout images for the current layout.
+def layout_path(p_identifier)
+  "/assets/layouts/#{@layouts.first.identifier[1..-2]}/#{p_identifier}"
+end
