5848f80db9146049229c1a050f90a6a89522b654
Josh Proehl <josh@daedalusdreams.com>
2022-10-04 09:04:26 -0500

    Upgrade to nanoc4/ruby3

--- Gemfile
+++ Gemfile
@@ -1,12 +1,14 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
-gem 'nanoc', '3.7.2'
+gem 'nanoc', '~> 4.12'
 gem 'guard-nanoc'
 gem 'guard-bundler'
 gem 'adsf'
-gem 'haml'
+gem 'haml', '~> 5.1.2'
 gem 'kramdown'
 gem 'sass'
 gem 'systemu'
 gem 'builder' #For XML Sitemap
+
+gem "webrick", "~> 1.7"
--- lib/default.rb
+++ lib/default.rb
@@ -6,7 +6,7 @@ include Nanoc::Helpers::XMLSitemap
 
 # An easy way to get a link to layout images for the current layout.
 def layout_path(p_identifier)
-  "/assets/layouts/#{@layouts.first.identifier[1..-2]}/#{p_identifier}"
+  "/assets/layouts#{@layouts.first.identifier.without_ext}/#{p_identifier}"
 end
 
 
